31f935b23b5a734f94b8e26f0b4aa11d76ba0539
Cindy Watters <114846194+cindyzwatters@users.noreply.github.com>
2023-05-21 22:22:26 -0400

    Entertainment.java

--- Entertainment.java
+++ Entertainment.java
@@ -1,7 +1,3 @@
-// Cindy Watters
-// 2.5 Programming Assignment
-// CIS 2503
-// 05/9/2023
 
 
 package com.smarthome2;
@@ -48,8 +44,3 @@ public class Entertainment extends Device {
     
 }
 
-
-//The code for this programming assignment for Advanced Java,
-//SmartHome2, is my own original submission (unless otherwise cited) in accordance with
-//the Academic Honesty Policy of the Florida Institute of Technology.
-//Faithfully submitted by Cindy Watters, on 5/5/2023.
