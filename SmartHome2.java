972d22fbeda0d0e1da2863cc32adb00f53961b07
Cindy Watters <114846194+cindyzwatters@users.noreply.github.com>
2023-05-21 22:20:31 -0400

    SmartHome2.java

--- SmartHome2.java
+++ SmartHome2.java
@@ -1,8 +1,3 @@
-// Cindy Watters
-// 2.5 Programming Assignment
-// CIS 2503
-// 05/12/2023
-
 
 package com.smarthome2;
 
@@ -340,9 +335,3 @@ public class SmartHome2 {
         }
     }
 }
-
-
-//The code for this programming assignment for Advanced Java,
-//SmartHome2, is my own original submission (unless otherwise cited) in accordance with
-//the Academic Honesty Policy of the Florida Institute of Technology.
-//Faithfully submitted by Cindy Watters, on 5/10/2023.
